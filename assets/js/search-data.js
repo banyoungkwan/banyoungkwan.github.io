32bdc260e414f229185a037097df0cbcb3774be8
banyoungkwan <46808508+banyoungkwan@users.noreply.github.com>
2025-07-31 09:03:04 +0000

    Deploying to gh-pages from @ banyoungkwan/banyoungkwan.github.io@3265a09a1ec832673dcc4164f7d772da038d4198 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-submenus",
-          title: "submenus",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/_pages/dropdown/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
